08ff958fac79def01a3bd7025740874e8ff952f9
Aaron Escalona <escalona.ap@gmail.com>
2012-12-27 10:16:14 -0800

    Added pagination to links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def index
-    @links = Link.all
+    @links = Link.paginate(:page => params[:page], :per_page => params[:per_page])
   end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,6 @@
 class Link < ActiveRecord::Base
+  self.per_page = 25
+
   belongs_to :user
   attr_accessible :title, :url
 end
